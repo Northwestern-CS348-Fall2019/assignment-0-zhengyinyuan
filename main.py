8f947b7649b78d66ad1e59946aaa18fa7b75d0a8
Yinyuan Zheng <zhengyinyuan@u.northwestern.edu>
2019-09-27 17:10:46 -0500

    Return T

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import student_code
 class SetupTest(unittest.TestCase):
     def test1(self):
     	result = student_code.hello_world()
+    	print(result)
     	self.assertTrue(result)
 
 if __name__ == '__main__':
